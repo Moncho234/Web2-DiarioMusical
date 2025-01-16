e57bc90a20d80fe5caa24e169d809b7d2bf8b7f4
Moncho234 <156394606+Moncho234@users.noreply.github.com>
2025-01-15 21:08:11 -0700

    Delete backend directory

--- backend/crud.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session  # Importa la clase para manejar sesiones de base de datos.
-from . import models, schemas  # Importa los modelos y esquemas definidos en el proyecto.
-
-# Función para obtener todos los usuarios (actualmente no se usa en este proyecto).
-def get_users(db: Session):
-    return db.query(models.User).all()  # Consulta todas las filas de la tabla "users".
